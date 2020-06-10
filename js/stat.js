e65bf41bb7c6516b4dc29e5da698951f50678d72
Julia Queen <juliaqueenking@Queen.local>
2020-06-10 17:47:01 +0300

    Рандомность цвета в переменную и читаемость кода

--- js/stat.js
+++ js/stat.js
@@ -9,6 +9,8 @@ var FONT_GAP = 15;
 var BAR_WIDTH = 40;
 var BAR_HEIGHT = 150;
 var BAR_GAP = 50;
+var BAR_DOWN = 30;
+var TEXT_COLOR = '#000'
 
 
 var renderCloud = function (ctx, x, y, color) {
@@ -28,6 +30,10 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
+var getRandomBlueColor = function (player) {
+  return 'hsl(240, ' + Math.floor(Math.random() * 100) + '%, 50%)';
+}
+
 window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
@@ -41,17 +47,15 @@ window.renderStatistics = function (ctx, players, times) {
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
-    ctx.fillStyle = '#000';
-    ctx.fillText(players[i], CLOUD_X + GAP + BAR_WIDTH + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - FONT_GAP - FONT_GAP);
-    ctx.fillText(Math.round(times[i]), CLOUD_X + GAP + BAR_WIDTH + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - GAP - GAP - GAP - FONT_GAP - (BAR_HEIGHT * times[i]) / maxTime - GAP - GAP);
-    ctx.fillRect(CLOUD_X + GAP + BAR_WIDTH + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - GAP - GAP - GAP - GAP - (BAR_HEIGHT * times[i]) / maxTime, BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
+    ctx.fillStyle = TEXT_COLOR;
+    ctx.fillText(players[i], CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - BAR_DOWN);
+    ctx.fillText(Math.round(times[i]), CLOUD_X + GAP + BAR_WIDTH + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - BAR_GAP - (BAR_HEIGHT * times[i]) / maxTime - GAP);
 
     if (players[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      ctx.fillRect(CLOUD_X + GAP + BAR_WIDTH + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - GAP - GAP - GAP - GAP - (BAR_HEIGHT * times[i]) / maxTime, BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
     } else {
-      ctx.fillStyle = 'hsl(240, ' + (i + 20) * i + '%, 50%)';
-      ctx.fillRect(CLOUD_X + GAP + BAR_WIDTH + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - GAP - GAP - GAP - GAP - (BAR_HEIGHT * times[i]) / maxTime, BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
+      ctx.fillStyle = getRandomBlueColor();
     }
+    ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - BAR_WIDTH - (BAR_HEIGHT * times[i]) / maxTime, BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
   }
 };
